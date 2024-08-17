422b24639e0c04f38de1bf7e4f0b6bdf01a8522c
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-08-17 02:45:09 +0100

    "Added Image import and updated AuthLayout component with new div structure and background image"

--- netflix/app/(auth)/layout.tsx
+++ netflix/app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import { ReactNode } from "react";
-
+import bgImg from "../../public/images/hero.jpg";
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <h1>hello</h1>
+    <div className="relative flex w-screen h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src={bgImg}
+        alt="backgound image"
+        className="hidden sm:flex sm:object-cover -z-10"
+        priority
+        fill
+      />
       {children}
-    </>
+    </div>
   );
 };
 
