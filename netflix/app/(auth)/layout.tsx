327a00b8e94c0ae5d4e5c92752aa9a2b2fd3ed5b
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-08-17 03:03:34 +0100

    "Added logo image to auth layout and updated background image styling"

--- netflix/app/(auth)/layout.tsx
+++ netflix/app/(auth)/layout.tsx
@@ -1,15 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Image from "next/image";
 import { ReactNode } from "react";
 import bgImg from "../../public/images/hero.jpg";
+import Logo from "../../public/images/logo.png";
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative flex w-screen h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Image
         src={bgImg}
         alt="backgound image"
-        className="hidden sm:flex sm:object-cover -z-10"
+        className="hidden sm:flex sm:object-cover -z-10 brightness-50"
         priority
         fill
+      />
+      <Image
+        alt="logo"
+        src={Logo}
+        width={120}
+        height={120}
+        className="absolute left-4  top-4 object-contain md:left-10 md:top-6 "
+        priority
       />
       {children}
     </div>
