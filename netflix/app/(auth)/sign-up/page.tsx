2d16aa3118ad13e22b4d66b8c75751a20d5683c0
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-08-17 03:15:24 +0100

    "Added imports for Button and Input components, replaced placeholder SignUp component with a form containing email input and submit button"

--- netflix/app/(auth)/sign-up/page.tsx
+++ netflix/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14 ">
+      <form action="">
+        <h2 className="text-3xl font-semibold  text-white">Sign Up</h2>
+        <div className="space-y-4 mt-5">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+          />
+          <Button
+            type="submit"
+            variant="destructive"
+            className="w-full bg-[#e50914] "
+          >
+            Sign up
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default SignUp;
