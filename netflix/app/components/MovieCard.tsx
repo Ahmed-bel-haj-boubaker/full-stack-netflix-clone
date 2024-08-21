ac4a83592afae961f87ab04a9e1d547fe798449f
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2024-08-21 02:24:15 +0100

    "Added MovieVideo component, integrated it into HomePage, and updated next.config.js for image handling"

---
+++ netflix/app/components/MovieCard.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { HeartIcon, PlayCircle } from "lucide-react";
+
+interface iAppProps {
+  title: string;
+  overview: string;
+  movieId: number;
+  watchList: boolean;
+  watchListId: string;
+  youtubeUrl: string;
+  age: number;
+  year: number;
+  time: number;
+}
+
+const MovieCard = ({
+  title,
+  overview,
+  age,
+  year,
+  time,
+  movieId,
+  watchList,
+  watchListId,
+  youtubeUrl,
+}: iAppProps) => {
+  return (
+    <>
+      <button className="-mt-14">
+        <PlayCircle className="size-20" />
+      </button>
+      <div className="right-5 top-5 absolute z-10">
+        {watchList ? (
+          <form>
+            <Button variant="outline" size="icon">
+              <HeartIcon className="size-4 text-red-500" />
+            </Button>
+          </form>
+        ) : (
+          <form>
+            <Button variant="outline" size="icon">
+              <HeartIcon className="size-4  " />
+            </Button>
+          </form>
+        )}
+      </div>
+      <div className="p-5 absolute bottom-0 left-0">
+        <h1 className="font-bold text-lg line-clamp-1 ">{title}</h1>
+        <div className="flex gap-x-2 items-center">
+          <p className="font-normal text-sm ">{year}</p>
+          <p className="font-normal border py-0.5 px-1 border-gray-200 rounded text-sm">
+            {age}+
+          </p>
+          <p className="font-normal text-sm">{time}h</p>
+        </div>
+        <p>{overview}</p>
+      </div>
+    </>
+  );
+};
+
+export default MovieCard;
---
+++ netflix/app/components/RecentlyAdded.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import prisma from "../utils/db";
+import MovieCard from "./MovieCard";
+
+async function getData() {
+  const data = await prisma.movie.findMany({
+    select: {
+      title: true,
+      overview: true,
+      youtubeString: true,
+      imageString: true,
+      WatchLists: true,
+      id: true,
+      age: true,
+      release: true,
+      duration: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 4,
+  });
+  return data;
+}
+
+const RecentlyAdded = async () => {
+  const data = await getData();
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
+      {data.map((e) => (
+        <div key={e.id} className="relative h-48">
+          <Image
+            src={e.imageString}
+            alt="Movie"
+            width={500}
+            height={400}
+            className="rounded-sm absolute w-full h-full"
+          />
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center">
+              {" "}
+              <Image
+                src={e.imageString}
+                alt="Movie"
+                width={800}
+                height={800}
+                className="rounded-lg  object-cover absolute w-full h-full -z-10"
+              />
+              <MovieCard
+                title={e.title}
+                overview={e.overview}
+                movieId={e.id}
+                watchListId={e.WatchLists[0]?.id}
+                youtubeUrl={e.youtubeString}
+                watchList={e.WatchLists.length > 0 ? true : false}
+                key={e.id}
+                age={e.age}
+                year={e.release}
+                time={e.duration}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecentlyAdded;
